2362ebcd8657c35f16fb2ca5ca3c91834d30ff6e
eurbasius <eur@whiteaway.com>
2021-05-15 22:14:15 +0200

    removing delay between sounds when you click on a button in JS drum kit task.

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,20 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+if (navigator.getUserMedia) {
+  // do stuff
+  navigator.getUserMedia({video:true, audio:false}, success, error);
+}
+else {
+  console.log('Lel!');
+}
+
+function success(stream) {
+  console.log(stream)
+  document.getElementById('myVideo').src = stream;
+}
+
+function error(stream) {
+  console.log('ERROR!');
+}
